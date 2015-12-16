1dbfa42e9ee5e17bddd335341d0aea7657ffcea9
henk <info@metaclass.nl>
2015-12-16 19:12:52 +0100

    Revert to targeting Symfony 2.6-2.8 - UsernamePasswordFormAuthenticationGuard - composer.json - Corrected README.md (Requirements)

--- README.md
+++ README.md
@@ -26,7 +26,8 @@ FEATURES
 
 REQUIREMENTS
 ------------
-This bundle is for the symfony framework and requires Symfony >=2.3 and <3.0.
+This bundle is for the symfony framework and requires Symfony >=2.6 and <3.0-dev.
+(for Symfony ~2.3 use v0.3)
 Requires metaclass-nl/tresholds-governor 0.2@dev which uses Doctrine DBAL >=2.3
 Tested with MySQL 5.5.
 
--- Service/UsernamePasswordFormAuthenticationGuard.php
+++ Service/UsernamePasswordFormAuthenticationGuard.php
@@ -17,8 +17,9 @@ use Symfony\Component\Security\Http\HttpUtils;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
-use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Security\Core\Security;
 
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\AuthenticationServiceException;
@@ -40,7 +41,7 @@ class UsernamePasswordFormAuthenticationGuard extends AbstractAuthenticationList
     /**
      * {@inheritdoc}
      */
-    public function __construct(SecurityContextInterface $tokenStorage, AuthenticationManagerInterface $authenticationManager, SessionAuthenticationStrategyInterface $sessionStrategy, HttpUtils $httpUtils, $providerKey, AuthenticationSuccessHandlerInterface $successHandler, AuthenticationFailureHandlerInterface $failureHandler, array $options = array(), LoggerInterface $logger = null, EventDispatcherInterface $dispatcher = null, CsrfProviderInterface $csrfProvider = null)
+    public function __construct(TokenStorageInterface $tokenStorage, AuthenticationManagerInterface $authenticationManager, SessionAuthenticationStrategyInterface $sessionStrategy, HttpUtils $httpUtils, $providerKey, AuthenticationSuccessHandlerInterface $successHandler, AuthenticationFailureHandlerInterface $failureHandler, array $options = array(), LoggerInterface $logger = null, EventDispatcherInterface $dispatcher = null, CsrfProviderInterface $csrfProvider = null)
     {
         parent::__construct($tokenStorage, $authenticationManager, $sessionStrategy, $httpUtils, $providerKey, $successHandler, $failureHandler, array_merge(array(
                 'username_parameter' => '_username',
@@ -95,7 +96,7 @@ class UsernamePasswordFormAuthenticationGuard extends AbstractAuthenticationList
         $exception = null;
         $originalCred = $this->getCredentials($request);
         $filteredCred = $this->filterCredentials($originalCred);
-        $request->getSession()->set(SecurityContextInterface::LAST_USERNAME, $originalCred[0]);
+        $request->getSession()->set(Security::LAST_USERNAME, $originalCred[0]);
         
         if (null !== $this->csrfProvider) {
             $this->checkCrsfToken($request);
