e9b0bd8eebced925c30abf06e3356872f69c21ee
henk <info@metaclass.nl>
2016-01-13 16:30:25 +0100

    #20: Adapt to Symfony 3.0 Since Symfony 2.6 last conctructor argument may be a CsrfTokenManagerInterface Since 3.0 it is typehinted CsrfTokenManagerInterface - Service\UsernamePasswordFormAuthenticationGuard renamed  to     ::__construct replaced CsrfProviderInterface by CsrfTokenManagerInterface     ::checkCrsfToken replaced with code from UsernamePasswordFormAuthenticationListener

--- Service/UsernamePasswordFormAuthenticationGuard.php
+++ Service/UsernamePasswordFormAuthenticationGuard.php
@@ -7,7 +7,8 @@
 namespace Metaclass\AuthenticationGuardBundle\Service;
 
 use Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener;
-use Symfony\Component\Form\Extension\Csrf\CsrfProvider\CsrfProviderInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
@@ -20,6 +21,7 @@ use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\ParameterBagUtils;
 
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\AuthenticationServiceException;
@@ -30,7 +32,7 @@ use Metaclass\TresholdsGovernor\Service\TresholdsGovernor;
 
 class UsernamePasswordFormAuthenticationGuard extends AbstractAuthenticationListener {
     
-    protected $csrfProvider;
+    protected $csrfTokenManager;
     protected $myTokenStorage;
     
     protected $governor;
@@ -41,7 +43,7 @@ class UsernamePasswordFormAuthenticationGuard extends AbstractAuthenticationList
     /**
      * {@inheritdoc}
      */
-    public function __construct(TokenStorageInterface $tokenStorage, AuthenticationManagerInterface $authenticationManager, SessionAuthenticationStrategyInterface $sessionStrategy, HttpUtils $httpUtils, $providerKey, AuthenticationSuccessHandlerInterface $successHandler, AuthenticationFailureHandlerInterface $failureHandler, array $options = array(), LoggerInterface $logger = null, EventDispatcherInterface $dispatcher = null, CsrfProviderInterface $csrfProvider = null)
+    public function __construct(TokenStorageInterface $tokenStorage, AuthenticationManagerInterface $authenticationManager, SessionAuthenticationStrategyInterface $sessionStrategy, HttpUtils $httpUtils, $providerKey, AuthenticationSuccessHandlerInterface $successHandler, AuthenticationFailureHandlerInterface $failureHandler, array $options = array(), LoggerInterface $logger = null, EventDispatcherInterface $dispatcher = null, CsrfTokenManagerInterface $csrfTokenManager = null)
     {
         parent::__construct($tokenStorage, $authenticationManager, $sessionStrategy, $httpUtils, $providerKey, $successHandler, $failureHandler, array_merge(array(
                 'username_parameter' => '_username',
@@ -51,7 +53,7 @@ class UsernamePasswordFormAuthenticationGuard extends AbstractAuthenticationList
                 'post_only'          => true,
         ), $options), $logger, $dispatcher);
     
-        $this->csrfProvider = $csrfProvider;
+        $this->csrfTokenManager = $csrfTokenManager;
         $this->myTokenStorage = $tokenStorage;
     }
     
@@ -98,7 +100,7 @@ class UsernamePasswordFormAuthenticationGuard extends AbstractAuthenticationList
         $filteredCred = $this->filterCredentials($originalCred);
         $request->getSession()->set(Security::LAST_USERNAME, $originalCred[0]);
         
-        if (null !== $this->csrfProvider) {
+        if (null !== $this->csrfTokenManager) {
             $this->checkCrsfToken($request);
         }
         
@@ -163,9 +165,9 @@ class UsernamePasswordFormAuthenticationGuard extends AbstractAuthenticationList
     
     protected function checkCrsfToken(Request $request) 
     {
-        $csrfToken = $request->get($this->options['csrf_parameter'], null, true);
-    
-        if (false === $this->csrfProvider->isCsrfTokenValid($this->options['intention'], $csrfToken)) {
+        $csrfToken = ParameterBagUtils::getRequestParameterValue($request, $this->options['csrf_parameter']);
+
+        if (false === $this->csrfTokenManager->isTokenValid(new CsrfToken($this->options['csrf_token_id'], $csrfToken))) {
             throw new InvalidCsrfTokenException('Invalid CSRF token.');
         }
     }    
